8e0c3bfd95804232d190276a3f57506f7641217d
JohaBogda <bogdanowj@gmail.com>
2021-10-22 17:05:15 -0400

    Created Springboot for capstone project

---
+++ src/main/java/com/example/com/CapstoneFriesApplication.java
@@ -0,0 +1,13 @@
+package com.example.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneFriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneFriesApplication.class, args);
+	}
+
+}
